2826b59d96b420a49feac85303ece2fd05be2ce3
jleivaolmo <jleiva@laboratorioechevarne.com>
2025-04-17 12:59:43 +0200

    Update BusinessPartnerController.java

--- src/main/java/com/echevarne/s4dummy/controllers/BusinessPartnerController.java
+++ src/main/java/com/echevarne/s4dummy/controllers/BusinessPartnerController.java
@@ -19,4 +19,36 @@ public class BusinessPartnerController {
     }
 
     record DummyResponse(BusinessPartner d) {}
+    
+    @GetMapping("/$metadata")
+    public ResponseEntity<String> getMetadata() {
+        String metadataXml = """
+            <?xml version="1.0" encoding="utf-8"?>
+            <edmx:Edmx xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx" Version="4.0">
+              <edmx:DataServices>
+                <Schema xmlns="http://docs.oasis-open.org/odata/ns/edm" Namespace="API_BUSINESS_PARTNER">
+                  <EntityType Name="A_BusinessPartner">
+                    <Key>
+                      <PropertyRef Name="BusinessPartner"/>
+                    </Key>
+                    <Property Name="BusinessPartner" Type="Edm.String"/>
+                    <Property Name="FirstName" Type="Edm.String"/>
+                    <Property Name="LastName" Type="Edm.String"/>
+                    <Property Name="BusinessPartnerFullName" Type="Edm.String"/>
+                    <Property Name="BusinessPartnerCategory" Type="Edm.String"/>
+                    <Property Name="Gender" Type="Edm.String"/>
+                    <Property Name="Language" Type="Edm.String"/>
+                  </EntityType>
+                  <EntityContainer Name="container">
+                    <EntitySet Name="A_BusinessPartner" EntityType="API_BUSINESS_PARTNER.A_BusinessPartner"/>
+                  </EntityContainer>
+                </Schema>
+              </edmx:DataServices>
+            </edmx:Edmx>
+            """;
+
+        return ResponseEntity.ok()
+            .header("Content-Type", "application/xml")
+            .body(metadataXml);
+    }
 }
