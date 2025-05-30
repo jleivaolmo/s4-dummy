e81f6d8da9dc25c1be90411863ed3bffd8e7d4a2
jleivaolmo <jleiva@laboratorioechevarne.com>
2025-05-30 13:08:13 +0200

    webservice para obtener token de Google

---
+++ src/main/java/com/echevarne/s4dummy/controllers/TokenController.java
@@ -0,0 +1,53 @@
+package com.echevarne.s4dummy.controllers;
+
+import java.io.FileInputStream;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.IdTokenCredentials;
+import com.google.auth.oauth2.IdTokenProvider;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/token")
+@Slf4j
+public class TokenController {
+
+	@GetMapping(value = "/getToken")
+	public ResponseEntity<String> getToken() {
+		String jsonPath = "c://temp//pj-ma-host-prod-eb47479170a2.json";
+		String targetAudience = "https://recepcion-676620684522.europe-southwest1.run.app";
+		String idToken = null;
+
+		try {
+			// Crear credenciales con alcance (scope)
+			GoogleCredentials credentials = GoogleCredentials
+	                .fromStream(new FileInputStream(jsonPath));
+
+			if (!(credentials instanceof IdTokenProvider)) {
+	            throw new IllegalArgumentException("Credenciales no soportan ID tokens");
+	        }
+
+			IdTokenCredentials idTokenCredentials = IdTokenCredentials.newBuilder()
+	                .setIdTokenProvider((IdTokenProvider) credentials)
+	                .setTargetAudience(targetAudience)
+	                .build();
+
+	        idTokenCredentials.refresh();
+
+	        idToken = idTokenCredentials.getAccessToken().getTokenValue();
+
+			// Mostrar el token
+			log.info("Bearer token: " + idToken);
+			return new ResponseEntity<>(idToken, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+}
